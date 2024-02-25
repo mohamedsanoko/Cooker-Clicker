2ee82f1fc793c7dbbe35d2ecc1f395f8659c6db2
Mohamed <msanoko5@gmail.com>
2024-02-25 09:18:51 -0500

    Cookie clicking indefinetely, need to make it stop every five seconds.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -9,6 +10,12 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID, value="cookie")
-cookie.click()
+
+current_time = time.time()
+interval = 5
+
+while True:
+    cookie.click()
+
 
 
